5d45305a882d486f756b97163e26aaba036a9f08
Quang Minh <quangnm2209@gmail.com>
2024-03-20 19:55:57 +0700

    adding interact with flashlight

--- Assets/Internal/Scripts/Character/CharacterConfig.cs
+++ Assets/Internal/Scripts/Character/CharacterConfig.cs
@@ -2,6 +2,7 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class CharacterConfig : MonoBehaviour
 {
@@ -9,8 +10,24 @@ public class CharacterConfig : MonoBehaviour
     public Transform leftHand;
     public Transform head;
     public Transform hat;
-
-
     public Transform head_aim;
 
+    private Animator animator;
+
+    [Header("FlashLight")]
+    [SerializeField] private GameObject flashLight;
+    [SerializeField] private Rig hand_hold_rig;
+    [SerializeField] private int hand_hold_layer_index;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    public void FlashLightAction(bool v)
+    {
+        flashLight.SetActive(v);
+        hand_hold_rig.weight = v ? 1f : 0f;
+        animator.SetLayerWeight(hand_hold_layer_index, v ? 1f : 0f);
+    }
 }
---
+++ Assets/Internal/Scripts/Player/PlayerEquipment.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerEquipment : NetworkBehaviour
+{
+    private CharacterConfig characterConfig;
+    private bool interact = false;
+
+    private void Update()
+    {
+        if (!IsOwner)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            InteractWithFlashLight(interact);
+            interact = !interact;
+        }
+    }
+    public void InteractWithFlashLight(bool v)
+    {
+        if (characterConfig == null)
+        {
+            characterConfig = GetComponent<PlayerMovement>().GetCharacterConfig();
+        }
+        characterConfig.FlashLightAction(v);
+    }
+
+}
